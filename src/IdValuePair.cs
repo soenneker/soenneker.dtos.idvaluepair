2fd6520b77cfe15c2c1a2216a0fdb25ddeefe2a3
Jake Soenneker <jake@soenneker.com>
2025-02-17 14:18:22 -0600

    Initial commit

---
+++ src/IdValuePair.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Soenneker.Dtos.IdValuePair;
+
+/// <summary>
+/// A minimal Record type with an Id (string), Value (string)
+/// </summary>
+public record IdValuePair
+{
+    [Required]
+    [JsonPropertyName("id")]
+    public string Id { get; init; } = null!;
+
+    [Required]
+    [JsonPropertyName("value")]
+    public string Value { get; init; } = null!;
+}
---
+++ test/Soenneker.Dtos.IdValuePair.Tests/IdValuePairTests.cs
@@ -0,0 +1,21 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Dtos.IdValuePair.Tests;
+
+[Collection("Collection")]
+public class IdValuePairTests : FixturedUnitTest
+{
+
+
+    public IdValuePairTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
